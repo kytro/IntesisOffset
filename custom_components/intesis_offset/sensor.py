61b2b944a17aa42179425ddb26d5fa34d08ad5f5
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-05 07:25:22 +1000

    sensor

--- custom_components/intesis_offset/sensor.py
+++ custom_components/intesis_offset/sensor.py
@@ -6,6 +6,46 @@ from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_URL, CONF_DEV
 
 DOMAIN = "intesis_offset"
 
+class WebFetcher:
+    def __init__(self, url, username, password):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.page = None
+        self.browser = None
+        
+    async def login(self):
+        self.browser = await launch(headless=True)
+        self.page = await self.browser.newPage()
+        await self.page.goto(self.url)
+
+        # Replace 'username_selector' and 'password_selector' with the actual selectors
+        await self.page.type('input[name="signin[username]"]', username)
+        await self.page.type('input[name="signin[password]"]', password)
+
+        # Replace 'login_button_selector' with the actual selector
+        await self.page.click('input[type="submit"]')
+        await self.page.waitForNavigation()
+        
+        # Navigate to the next page
+        await page.goto('https://accloud.intesis.com/device/list')
+
+    async def fetch_data(self):
+        if self.page is None:
+            await self.login()
+
+        # Check if the page has the right elements
+        # Replace 'element_selector' with the actual selector
+        element = await self.page.querySelector('element_selector')
+        if element is None:
+            await self.login()
+
+        # Fetch data from the website
+        # Replace 'data_selector' with the actual selector
+        data = await self.page.querySelectorEval('data_selector', '(element) => element.textContent')
+
+        return data
+
 class IntesisOffsetSensor(Entity):
     def __init__(self, device, username, password, url):
         self._name = device['name']
@@ -14,6 +54,7 @@ class IntesisOffsetSensor(Entity):
         self._unique_id = device['entity_id']
         self._linked_entity_id = device['linked_entity_id']
         self._linked_entity_state = None
+        self._fetcher = WebFetcher(url, username, password)
         
     @property
     def name(self):
