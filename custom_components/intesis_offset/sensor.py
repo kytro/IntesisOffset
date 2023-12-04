56ec56abd0311b5ffe43da4e484041ddf64b6936
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-04 15:03:49 +1000

    sensor

--- custom_components/intesis_offset/sensor.py
+++ custom_components/intesis_offset/sensor.py
@@ -15,20 +15,6 @@ class IntesisOffsetSensor(Entity):
         self._linked_entity_id = device['linked_entity_id']
         self._linked_entity_state = None
         
-    async def async_added_to_hass(self):
-        """Run when entity about to be added."""
-        @callback
-        def update_state(event):
-            """Update the state."""
-            self._linked_entity_state = self.hass.states.get(self._linked_entity_id)
-
-        # Listen for when linked_entity_id has a state change
-        self.async_on_remove(
-            async_dispatcher_connect(
-                self.hass, f"entity_{self._linked_entity_id}_state_update", update_state
-            )
-        )
-
     @property
     def name(self):
         return self._name
@@ -51,9 +37,11 @@ class IntesisOffsetSensor(Entity):
         return self._unique_id
 
     @property
-    def linked_sensor_state(self):
-        """Return the state of the linked sensor."""
-        return self._linked_entity_state.state if self._linked_entity_state else None
+    def device_state_attributes(self):
+        """Return the state attributes of the sensor."""
+        return {
+            'linked_sensor_state': self.linked_sensor_state
+        }
         
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the sensor platform."""
