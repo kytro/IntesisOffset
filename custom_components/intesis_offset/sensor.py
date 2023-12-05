4171cfd7999d9f7130d634a2b0e1fd3979f27175
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-05 13:10:12 +1000

    offset

--- custom_components/intesis_offset/sensor.py
+++ custom_components/intesis_offset/sensor.py
@@ -19,7 +19,7 @@ class WebFetcher:
         self.browser = None
             
     async def login(self):
-        self.browser = await self._hass.async_add_executor_job(launch, headless=True)
+        self.browser = await self._hass.async_add_executor_job(launch(headless=True))
         #self.page = await self.browser.newPage()
         #await self.page.goto(self.url)
         
