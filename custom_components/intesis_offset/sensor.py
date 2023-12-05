ea0f70a4d9436bef72bacae1fe5557cf0b46bb85
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-06 06:32:44 +1000

    offset

--- custom_components/intesis_offset/sensor.py
+++ custom_components/intesis_offset/sensor.py
@@ -9,6 +9,8 @@ from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_URL, CONF_DEV
 
 DOMAIN = "intesis_offset"
 _LOGGER = logging.getLogger(__name__)
+
+
 class WebFetcher:
     def __init__(self, hass, url, username, password):
         self._hass = hass
@@ -17,56 +19,54 @@ class WebFetcher:
         self.password = password
         self.page = None
         self.browser = None
-            
+
     async def login(self):
-        self.browser = await _async_launch()
+        self.browser = await self._hass.async_add_executor_job(launch)
         self.page = await self.browser.newPage()
         await self.page.goto(self.url)
-        
+
         # Replace 'username_selector' and 'password_selector' with the actual selectors
-        await self.page.type('input[name="signin[username]"]', username)
-        await self.page.type('input[name="signin[password]"]', password)
+        await self.page.type('input[name="signin[username]"]', self.username)
+        await self.page.type('input[name="signin[password]"]', self.password)
 
         # Replace 'login_button_selector' with the actual selector
         await self.page.click('input[type="submit"]')
         await self.page.waitForNavigation()
-        
+
         # Navigate to the next page
-        await page.goto('https://accloud.intesis.com/device/list')
-        await self.update(page)
-        
+        await self.page.goto('https://accloud.intesis.com/device/list')
+        await self.update(self.page)
+
     async def fetch_data(self, device_name):
         if self.page is None:
             await self.login()
 
         # Check if the page has the right elements
-        #element = await self.page.querySelector('ul.devices')
-        #if element is None:
+        # element = await self.page.querySelector('ul.devices')
+        # if element is None:
         #    await self.login()
-            
+
         # Get the device IDs
-        #device_ids = await self.page.evaluate('''() => Array.from(document.querySelectorAll('ul.devices li.device span[id^="device_"]')).map(device => device.id)''')
-        
+        # device_ids = await self.page.evaluate('''() => Array.from(document.querySelectorAll('ul.devices li.device span[id^="device_"]')).map(device => device.id)''')
+
         # Find the correct device_id from device_ids using device_name
-        #device_id = next((id for id in device_ids if device_name in id), None)
-        #if device_id is None:
+        # device_id = next((id for id in device_ids if device_name in id), None)
+        # if device_id is None:
         #    raise ValueError(f"No device found with name {device_name}")
-        
+
         # Click on the device name to navigate to the device page
-        #await self.page.click(f"span[id='{device_id}_name']")
-        
+        # await self.page.click(f"span[id='{device_id}_name']")
+
         # Wait for the offset selector to be visible
-        #await self.page.waitForSelector("select#vtempOffset")
+        # await self.page.waitForSelector("select#vtempOffset")
 
         # Fetch data from the website
-        #data = await self.page.evaluate('''() => document.querySelector("select#vtempOffset").value''')
+        # data = await self.page.evaluate('''() => document.querySelector("select#vtempOffset").value''')
 
         return 0
 
-async def _async_launch():
-    browser = await launch()
-    return browser
-        
+
+
 class IntesisOffsetSensor(Entity):
     def __init__(self, hass, device, fetcher):
         self._hass = hass
@@ -76,20 +76,12 @@ class IntesisOffsetSensor(Entity):
         self._linked_entity_id = device['linked_entity_id']
         self._linked_entity_state = None
         self._fetcher = fetcher
-    
+
     async def async_init(self):
         self._state = await self.get_offset()
-    
+
     async def get_offset(self):
-        self._state =  await self._fetcher.fetch_data(self._name)
-    
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        return self._state
+        self._state = await self._fetcher.fetch_data(self._name)
 
     @property
     def name(self):
@@ -98,7 +90,15 @@ class IntesisOffsetSensor(Entity):
     @property
     def state(self):
         return self._state
-        
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
     @property
     def unique_id(self):
         """Return a unique ID."""
@@ -108,17 +108,18 @@ class IntesisOffsetSensor(Entity):
     def extra_state_attributes(self):
         """Return the state attributes of the sensor."""
         return {
-            'Linked Entity': self._linked_entity_id 
+            'Linked Entity': self._linked_entity_id
         }
-        
+
+
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the sensor platform."""
     # Get the configuration for this domain
     conf = hass.data[DOMAIN]
-    
+
     # Create a single WebFetcher for all devices
     fetcher = WebFetcher(hass, conf[CONF_URL], conf[CONF_USERNAME], conf[CONF_PASSWORD])
-    #await hass.async_add_executor_job(partial(sync_login, fetcher))
+    # await hass.async_add_executor_job(partial(sync_login, fetcher))
 
     # Create a sensor for each device
     sensors = []
