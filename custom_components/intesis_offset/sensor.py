9c9881a98215ceb39f3756abd4a3854aae25ffc2
Matt Creasey <matt.creasey@imosphere.co.uk>
2023-12-08 08:58:14 +1000

    set offset

--- custom_components/intesis_offset/sensor.py
+++ custom_components/intesis_offset/sensor.py
@@ -3,6 +3,7 @@ import aiohttp
 import asyncio
 
 from bs4 import BeautifulSoup
+from homeassistant.helpers import service
 from homeassistant.helpers.entity import Entity
 from homeassistant.core import callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
@@ -126,6 +127,73 @@ class IntesisWeb:
         offset = await self.navigate_to_device_and_get_offset(s, device_name)
         await s.close()
         return offset
+      
+    async def async_set_offset(self, device_name, offset):
+        """Set the offset."""
+        # Define the offset to value mapping based on the HTML
+        offset_to_value = {
+            -5: 65486,
+            -4: 65496,
+            -3: 65506,
+            -2: 65516,
+            -1: 65526,
+             0: 0,
+             1: 10,
+             2: 20,
+             3: 30,
+             4: 40,
+             5: 50
+        }
+
+        # Check if the offset is valid
+        if offset not in offset_to_value:
+            print(f"Invalid offset: {offset}. Offset must be between -5 and 5.")
+            return
+
+        # Get the corresponding value
+        value = offset_to_value[offset]
+        
+        # Get the device ID
+        device_id = await self.get_device_id(device_name)
+        
+        #uid - static
+        uid = 50002
+        
+        #used_id - static for now
+        used_id = 116771
+
+        # Create the URL
+        url = f"{self._base_url}/device/setVal?id={device_id}&uid={uid}&value={value}&userId={user_id}"
+
+        # Define the headers
+        headers = {
+            'Accept': 'text/html, */*; q=0.01',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Length': '0',
+            'Cookie': 'cb-enabled=accepted; symfonyFrontend=h44kn87uih0p8jvhdpfsqbk735; shortscc=18',
+            'DNT': '1',
+            'Host': 'accloud.intesis.com',
+            'Origin': 'https://accloud.intesis.com',
+            'Referer': 'https://accloud.intesis.com/device/list',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
+            'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"'
+        }
+
+        # Make the request
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers) as response:
+                if response.status == 200:
+                    return True
+                else:
+                    return False
         
         
 class IntesisOffsetSensor(Entity):
@@ -139,6 +207,15 @@ class IntesisOffsetSensor(Entity):
 
     async def async_init(self):
         self._state = await self._intesisWeb.async_get_offset(self._name)
+
+    async def async_set_offset(self, offset):
+        """Set the offset."""
+        if isinstance(offset, int) and -5 <= offset <= 5:
+            success = await self._intesisWeb.async_set_offset(self._name, offset)
+            if success:
+                self._state = offset
+                return True
+        return False
 
     @property
     def name(self):
@@ -179,4 +256,18 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         await sensor.async_init()
         sensors.append(sensor)
 
+    async def async_handle_set_offset(call):
+        """Handle the service call."""
+        entity_id = call.data.get('entity_id')
+        offset = call.data.get('offset')
+
+        entity = next((sensor for sensor in sensors if sensor.entity_id == entity_id), None)
+
+        if entity is not None:
+            success = await entity.async_set_offset(offset)
+            if not success:
+                _LOGGER.error("Failed to set offset for %s", entity_id)
+
+    hass.services.async_register(DOMAIN, 'set_offset', async_handle_set_offset)
+    
     async_add_entities(sensors, True)
